368d7b5e07f5e6fc51a1dc352b1c6ada00dfed6a
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2021-04-26 13:51:52 +0200

    done

--- src/main/java/cz/educanet/praha/MostAverageNumber.java
+++ src/main/java/cz/educanet/praha/MostAverageNumber.java
@@ -1,10 +1,28 @@
 package cz.educanet.praha;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MostAverageNumber {
 
     static int findMostAverageNumber(Scanner sc) {
-        return 0;
+        ArrayList<Integer> cisla = new ArrayList<>();
+        int soucet = 0;
+        int nejblizsi = 0;
+        while (sc.hasNext()) {
+            int input = sc.nextInt();
+            cisla.add(input);
+            soucet += input;
+        }
+        soucet /= cisla.size();
+
+
+        for (Integer integer : cisla) {
+            if (Math.abs(integer - soucet) <= Math.abs(nejblizsi - soucet)) {
+                nejblizsi = integer;
+            }
+        }
+
+        return nejblizsi;
     }
 }
