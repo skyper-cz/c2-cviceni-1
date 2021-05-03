85102a7dedcd783cae2739d46ae4f7b9c6315484
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2021-05-03 15:09:06 +0200

    h

--- src/main/java/cz/educanet/praha/SchoolAverageStudentsWithNameStartingWith.java
+++ src/main/java/cz/educanet/praha/SchoolAverageStudentsWithNameStartingWith.java
@@ -2,8 +2,11 @@ package cz.educanet.praha;
 
 import cz.educanet.praha.school.School;
 
+import java.util.ArrayList;
+
 public class SchoolAverageStudentsWithNameStartingWith {
     public static double averageAgeOfStudentsWithNameStartingWith(School s, char startsWith) {
+
         return 0;
     }
 }
--- src/main/java/cz/educanet/praha/SchoolCountAllStudents.java
+++ src/main/java/cz/educanet/praha/SchoolCountAllStudents.java
@@ -1,10 +1,20 @@
 package cz.educanet.praha;
 
 import cz.educanet.praha.school.School;
+import cz.educanet.praha.school.Class;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class SchoolCountAllStudents {
 
     public static int countAllStudents(School school) {
-        return 0;
+        int PocetStudentu = 0;
+        for (int i = 0; i < school.classes.stu; i++) {
+            PocetStudentu = school.classes.length;
+        }
+        return PocetStudentu;
     }
+
 }
