4fff2c7494fd5aed77abd9be83afdb5778225990
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2021-05-10 13:47:35 +0200

    Age done

--- src/main/java/cz/educanet/praha/SchoolAverageStudentsWithNameStartingWith.java
+++ src/main/java/cz/educanet/praha/SchoolAverageStudentsWithNameStartingWith.java
@@ -6,7 +6,19 @@ import java.util.ArrayList;
 
 public class SchoolAverageStudentsWithNameStartingWith {
     public static double averageAgeOfStudentsWithNameStartingWith(School s, char startsWith) {
-
-        return 0;
+        int prumernyVek = 0;
+        int celkovyVek = 0;
+        int pocetStudetu = 0;
+        for (int i = 0; i < s.classes.length; i++) {
+            for (int x = 0;x < s.classes[i].students.length; x++) {
+                String jmeno = s.classes[i].students[x].name;
+                if (startsWith == jmeno.charAt(0)) {
+                    celkovyVek += s.classes[i].students[x].age;
+                   pocetStudetu++;
+                }
+            }
+        }
+        prumernyVek = celkovyVek/pocetStudetu;
+        return prumernyVek;
     }
 }
